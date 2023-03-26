01ccf08bbb9dfcc30643b141f3be8474b9ffdf45
bybang <joshuaBang26@gmail.com>
2023-03-26 16:12:45 -0400

    add dependencies for the project

---
+++ screens/HomeScreen.tsx
@@ -0,0 +1,19 @@
+import { View, Text, SafeAreaView } from "react-native";
+
+const HomeScreen = () => {
+  return (
+    <SafeAreaView>
+      <Text>HomeScreen</Text>
+
+      {/* PRO/UPGRADE button */}
+
+      {/* Logo Image */}
+
+      {/* ActionRow */}
+      {/* ActionRow */}
+      {/* ActionRow */}
+    </SafeAreaView>
+  );
+};
+
+export default HomeScreen;
